5d9215160406fd06e87d9e8a9a9a8a728aff759d
GustavoOlimpio <gustavo-olimpio@hotmail.com>
2023-07-01 23:00:39 -0300

    task 1

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -1,22 +1,27 @@
 import { Address, Enrollment } from '@prisma/client';
 import { request } from '@/utils/request';
-import { invalidDataError, notFoundError } from '@/errors';
+import { invalidDataError, notFoundError , cannotEnrollBeforeStartDateError } from '@/errors';
 import addressRepository, { CreateAddressParams } from '@/repositories/address-repository';
 import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
 import { exclude } from '@/utils/prisma-utils';
 
-// TODO - Receber o CEP por parâmetro nesta função.
-async function getAddressFromCEP() {
-
-  // FIXME: está com CEP fixo!
-  const result = await request.get(`${process.env.VIA_CEP_API}/37440000/json/`);
-
+// TODO - Receber o CEP por parâmetro nesta função. OK
+async function getAddressFromCEP(cep:string) { 
+  // FIXME: está com CEP fixo! OK
+  const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
+  console.log()
   if (!result.data) {
     throw notFoundError();
   }
-
-  // FIXME: não estamos interessados em todos os campos
-  return result.data;
+  // FIXME: não estamos interessados em todos os campos OK
+  return {
+    logradouro: result.data.logradouro,
+    complemento:  result.data.complemento,
+    bairro:  result.data.bairro,
+    cidade:  result.data.localidade,
+    uf:  result.data.uf,
+    erro: result.data.erro
+  } ;
 }
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
@@ -46,6 +51,11 @@ type GetAddressResult = Omit<Address, 'createdAt' | 'updatedAt' | 'enrollmentId'
 async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollmentWithAddress) {
   const enrollment = exclude(params, 'address');
   const address = getAddressForUpsert(params.address);
+  
+  const cepIsCorrect = await getAddressFromCEP(address.cep);
+  if(cepIsCorrect.erro){
+    throw cannotEnrollBeforeStartDateError();
+  }
 
   // TODO - Verificar se o CEP é válido antes de associar ao enrollment.
 
